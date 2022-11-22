a3a3d8236ea77fafc2245febe0766e7ded6978e3
Unique One <uniquewon@gmail.com>
2022-11-22 16:11:00 +1100

    GenerateQuadUVs now allows for any vertex order.

--- GenerateQuadUVs/Editor/GenerateQuadUVs.cs
+++ GenerateQuadUVs/Editor/GenerateQuadUVs.cs
@@ -6,6 +6,8 @@ using UnityEditor;
 using UnityEngine;
 using System;
 using static ProgressBarAPI.API;
+using System.Linq;
+using System.Collections;
 
 namespace GenerateQuadUVs
 {
@@ -16,19 +18,32 @@ namespace GenerateQuadUVs
         {
             GetWindow<GenerateQuadUVs>("GenerateQuadUVs").Show();
         }
-        
+
         private bool _flipX = false;
         private bool _flipY = false;
-        private bool _invertedUVs = false;
         private bool _swapXY = false;
+        private int _vertOrder = 0;
+
+        List<Vector2> uvs = new List<Vector2> { new Vector2(0, 1), new Vector2(1, 1), new Vector2(1, 0), new Vector2(0, 0) };
+        List<List<Vector2>> uv_combinations = new List<List<Vector2>>();
+        List<string> uv_combination_names = new List<string>();
 
         void OnGUI()
         {
             _flipX = EditorGUILayout.Toggle("Flip X?", _flipX);
             _flipY = EditorGUILayout.Toggle("Flip Y?", _flipY);
-            _invertedUVs = EditorGUILayout.Toggle("Inverted UVs?", _invertedUVs);
             _swapXY = EditorGUILayout.Toggle("Swap X and Y coordinates?", _swapXY);
 
+            uv_combinations.Clear();
+            Permute<Vector2>(uvs.ToArray(), uv_combinations);
+
+            uv_combination_names.Clear();
+            for (int i = 0; i < uv_combinations.Count; i++)
+            {
+                uv_combination_names.Add("Order " + i.ToString());
+            }
+
+            _vertOrder = EditorGUILayout.Popup("Vertex Order", _vertOrder, uv_combination_names.ToArray());
 
             if (GUILayout.Button("Generate Quad UVs"))
             {
@@ -64,32 +79,53 @@ namespace GenerateQuadUVs
                         Mesh mesh = gameObject.GetComponent<MeshFilter>().sharedMesh;
                         //Vector3[] vertices = mesh.vertices;
                         List<Vector2> uvs = new List<Vector2>();
-                        
+
                         var numVerts = mesh.vertices.Length;
-                        
+
                         if (numVerts != 4 && numVerts != 8)
                         {
                             Debug.Log("This mesh on gameobject " + Selection.gameObjects[i].name + " does not seem to be a quad or dual sided quad, skipping.");
                             continue;
                         }
-                        
+
                         bool is2sided = (numVerts == 8) ? true : false;
 
                         Vector2 uv0, uv1, uv2, uv3;
-                        
-                        if (_invertedUVs)
+
+                        List<Vector2> useUVs = uv_combinations[_vertOrder];
+                        uv0 = new Vector2(useUVs[0].x, useUVs[0].y);
+                        uv1 = new Vector2(useUVs[1].x, useUVs[1].y);
+                        uv2 = new Vector2(useUVs[2].x, useUVs[2].y);
+                        uv3 = new Vector2(useUVs[3].x, useUVs[3].y);
+
+                        if (_flipX)
                         {
-                            uv3 = new Vector2(0, _flipY ? -1 : 1);
-                            uv2 = new Vector2(_flipX ? -1 : 1, _flipY ? -1 : 1);
-                            uv1 = new Vector2(_flipX ? -1 : 1, 0);
-                            uv0 = new Vector2(0, 0);
+                            if (uv0.x == 1.0f)
+                                uv0.x = -1.0f;
+
+                            if (uv1.x == 1.0f)
+                                uv1.x = -1.0f;
+
+                            if (uv2.x == 1.0f)
+                                uv2.x = -1.0f;
+
+                            if (uv3.x == 1.0f)
+                                uv3.x = -1.0f;
                         }
-                        else
+
+                        if (_flipY)
                         {
-                            uv0 = new Vector2(0, _flipY ? -1 : 1);
-                            uv1 = new Vector2(_flipX ? -1 : 1, _flipY ? -1 : 1);
-                            uv2 = new Vector2(_flipX ? -1 : 1, 0);
-                            uv3 = new Vector2(0, 0);
+                            if (uv0.y == 1.0f)
+                                uv0.y = -1.0f;
+
+                            if (uv1.y == 1.0f)
+                                uv1.y = -1.0f;
+
+                            if (uv2.y == 1.0f)
+                                uv2.y = -1.0f;
+
+                            if (uv3.y == 1.0f)
+                                uv3.y = -1.0f;
                         }
 
                         if (_swapXY)
@@ -99,12 +135,12 @@ namespace GenerateQuadUVs
                             uv2 = SwapXY(uv2);
                             uv3 = SwapXY(uv3);
                         }
-                        
+
                         uvs.Add(uv0);
                         uvs.Add(uv1);
                         uvs.Add(uv2);
                         uvs.Add(uv3);
-                        
+
                         if (is2sided)
                         {// This mesh has 8 verts, assume it is double sided, and add a second set of quad uv's.
                             uvs.Add(uv0);
@@ -116,45 +152,66 @@ namespace GenerateQuadUVs
                         mesh.SetUVs(0, uvs);
                         mesh.UploadMeshData(false);
                     }
-                    
+
                     if (gameObject.GetComponent<SkinnedMeshRenderer>())
                     {
                         Mesh mesh = gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh;
                         //Vector3[] vertices = mesh.vertices;
                         List<Vector2> uvs = new List<Vector2>();
-                        
+
                         var numVerts = mesh.vertices.Length;
-                        
+
                         if (numVerts != 4 && numVerts != 8)
                         {
                             Debug.Log("This mesh on gameobject " + Selection.gameObjects[i].name + " does not seem to be a quad or dual sided quad, skipping.");
                             continue;
                         }
-                        
+
                         bool is2sided = (numVerts == 8) ? true : false;
 
                         Vector2 uv0, uv1, uv2, uv3;
-                        
-                        if (_invertedUVs)
+
+                        List<Vector2> useUVs = uv_combinations[_vertOrder];
+                        uv0 = new Vector2(useUVs[0].x, useUVs[0].y);
+                        uv1 = new Vector2(useUVs[1].x, useUVs[1].y);
+                        uv2 = new Vector2(useUVs[2].x, useUVs[2].y);
+                        uv3 = new Vector2(useUVs[3].x, useUVs[3].y);
+
+                        if (_flipX)
                         {
-                            uv3 = new Vector2(0, _flipY ? -1 : 1);
-                            uv2 = new Vector2(_flipX ? -1 : 1, _flipY ? -1 : 1);
-                            uv1 = new Vector2(_flipX ? -1 : 1, 0);
-                            uv0 = new Vector2(0, 0);
+                            if (uv0.x == 1.0f)
+                                uv0.x = -1.0f;
+
+                            if (uv1.x == 1.0f)
+                                uv1.x = -1.0f;
+
+                            if (uv2.x == 1.0f)
+                                uv2.x = -1.0f;
+
+                            if (uv3.x == 1.0f)
+                                uv3.x = -1.0f;
                         }
-                        else
+
+                        if (_flipY)
                         {
-                            uv0 = new Vector2(0, _flipY ? -1 : 1);
-                            uv1 = new Vector2(_flipX ? -1 : 1, _flipY ? -1 : 1);
-                            uv2 = new Vector2(_flipX ? -1 : 1, 0);
-                            uv3 = new Vector2(0, 0);
+                            if (uv0.y == 1.0f)
+                                uv0.y = -1.0f;
+
+                            if (uv1.y == 1.0f)
+                                uv1.y = -1.0f;
+
+                            if (uv2.y == 1.0f)
+                                uv2.y = -1.0f;
+
+                            if (uv3.y == 1.0f)
+                                uv3.y = -1.0f;
                         }
-                        
+
                         uvs.Add(uv0);
                         uvs.Add(uv1);
                         uvs.Add(uv2);
                         uvs.Add(uv3);
-                        
+
                         if (is2sided)
                         {// This mesh has 8 verts, assume it is double sided, and add a second set of quad uv's.
                             uvs.Add(uv0);
@@ -180,5 +237,60 @@ namespace GenerateQuadUVs
 
             ProgressBarEnd();
         }
+
+        /*
+        public static List<List<Vector2>> GetCombination(List<Vector2> lst, int index, int count)
+        {
+            List<List<Vector2>> combinations = new List<List<Vector2>>();
+            List<Vector2> comb;
+            if (count == 0 || index == lst.Count)
+            {
+                return null;
+            }
+            for (int i = index; i < lst.Count; i++)
+            {
+                comb = new List<Vector2>();
+                comb.Add(lst.ElementAt(i));
+                combinations.Add(comb);
+                var rest = GetCombination(lst, i + 1, count - 1);
+                if (rest != null)
+                {
+                    foreach (var item in rest)
+                    {
+                        combinations.Add(comb.Union(item).ToList());
+                    }
+                }
+            }
+            return combinations;
+        }*/
+
+        public static void Permute<T>(T[] items, List<List<T>> output)
+        {
+            Permute(items, 0, new T[items.Length], new bool[items.Length], output);
+        }
+
+        private static void Permute<T>(T[] items, int item, T[] permutation, bool[] used, List<List<T>> output)
+        {
+            for (int i = 0; i < items.Length; ++i)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    permutation[item] = items[i];
+
+                    if (item < (items.Length - 1))
+                    {
+                        Permute(items, item + 1, permutation, used, output);
+                    }
+                    else
+                    {
+                        output.Add(permutation.ToList());
+                    }
+
+                    used[i] = false;
+                }
+            }
+        }
     }
 }
+
